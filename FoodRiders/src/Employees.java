960d51170b17adde1fdbcb351cd0c94f57d07828
Panos <Panos@DESKTOP-GQH30VD>
2018-03-18 22:22:36 +0200

    Employees class

---
+++ FoodRiders/src/Employees.java
@@ -0,0 +1,19 @@
+
+public class Employees {
+	private int code;
+	private String name;
+	private String position;
+	private int age;
+	private String recruitmentDate;
+	
+	public Employees(int code, String name, String position, int age, String recruitmentDate) {
+		this.code = code;
+		this.name = name;
+		this.position = position;
+		this.age = age;
+		this.recruitmentDate = recruitmentDate;
+	}
+	
+	
+
+}
