67534dbef28baa0e75364e835a787f0886e417dc
Panos <Panos@DESKTOP-GQH30VD>
2018-03-18 17:54:47 +0200

    First commit

---
+++ FoodRiders/src/guiEmployees_Package/GuiEmployees.java
@@ -0,0 +1,89 @@
+package guiEmployees_Package;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.BoxLayout;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+
+public class GuiEmployees {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void EmployeesScreen() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GuiEmployees window = new GuiEmployees();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GuiEmployees() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1075, 292);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 1057, 206);
+		frame.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{new Integer(1), "\u0396\u03AE\u03C3\u03B7\u03C2 \u0394\u03B7\u03BC\u03AE\u03C4\u03C1\u03B9\u03BF\u03C2", "\u039F\u03B4\u03B7\u03B3\u03CC\u03C2", new Integer(12), "1/1/2017"},
+				{new Integer(2), "\u03A3\u03C9\u03C4\u03B7\u03C1\u03AC\u03BA\u03B7\u03C2 \u03A0\u03B7\u03B3\u03B9\u03CE\u03C4\u03B7\u03C2", "\u03A8\u03AE\u03C3\u03C4\u03B7\u03C2", new Integer(33), "1/1/2017"},
+				{new Integer(3), "\u03A0\u03B1\u03BD\u03B1\u03B3\u03B9\u03CE\u03C4\u03B7\u03C2 \u039A\u03B1\u03C4\u03C4\u03AF\u03B4\u03B7\u03C2", "Manager", new Integer(34), null},
+			},
+			new String[] {
+				"\u039A\u03C9\u03B4\u03B9\u03BA\u03CC\u03C2 \u03A5\u03C0\u03B1\u03BB\u03BB\u03AE\u03BB\u03BF\u03C5", "\u039F\u03BD\u03BF\u03BC\u03B1\u03C4\u03B5\u03C0\u03CE\u03BD\u03C5\u03BC\u03BF", "\u039A\u03B1\u03B8\u03AE\u03BA\u03BF\u03BD\u03C4\u03B1", "\u0397\u03BB\u03B9\u03BA\u03AF\u03B1", "\u0397\u03BC\u03B5\u03C1\u03BF\u03BC\u03B7\u03BD\u03AF\u03B1 \u03A0\u03C1\u03CC\u03C3\u03BB\u03B7\u03C8\u03B7\u03C2"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Integer.class, String.class, String.class, Integer.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setPreferredWidth(130);
+		table.getColumnModel().getColumn(1).setPreferredWidth(130);
+		table.getColumnModel().getColumn(2).setPreferredWidth(130);
+		table.getColumnModel().getColumn(4).setPreferredWidth(145);
+		scrollPane.setViewportView(table);
+		
+		JButton btnProsthiki = new JButton("Προσθήκη Υπαλλήλου");
+		btnProsthiki.setBounds(10, 207, 153, 25);
+		frame.getContentPane().add(btnProsthiki);
+		
+		JButton btnDiagrafi = new JButton("Διαγραφή Υπαλλήλου");
+		btnDiagrafi.setBounds(175, 207, 153, 25);
+		frame.getContentPane().add(btnDiagrafi);
+		
+		JButton btnEpeksergasia = new JButton("Επξεργασία Υπαλλήλου");
+		btnEpeksergasia.setBounds(340, 207, 161, 25);
+		frame.getContentPane().add(btnEpeksergasia);
+	}
+}
---
+++ FoodRiders/src/guiMenu_Package/GuiMenu.java
@@ -0,0 +1,143 @@
+package guiMenu_Package;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import guiEmployees_Package.GuiEmployees;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JSeparator;
+
+public class GuiMenu {
+
+	private JFrame frmFoodRiders;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GuiMenu window = new GuiMenu();
+					window.frmFoodRiders.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GuiMenu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmFoodRiders = new JFrame();
+		frmFoodRiders.setResizable(false);
+		frmFoodRiders.setTitle("Food Riders");
+		frmFoodRiders.setBounds(100, 100, 690, 461);
+		frmFoodRiders.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{432, 0};
+		gridBagLayout.rowHeights = new int[]{16, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		frmFoodRiders.getContentPane().setLayout(gridBagLayout);
+		
+		JLabel label = new JLabel("\u039A\u03B5\u03BD\u03C4\u03C1\u03B9\u03BA\u03CC \u039C\u03B5\u03BD\u03BF\u03CD");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.fill = GridBagConstraints.VERTICAL;
+		gbc_label.insets = new Insets(0, 0, 5, 0);
+		gbc_label.gridx = 0;
+		gbc_label.gridy = 0;
+		frmFoodRiders.getContentPane().add(label, gbc_label);
+		
+		JButton btnTrexousa = new JButton("\u03A4\u03C1\u03AD\u03C7\u03BF\u03C5\u03C3\u03B1 \u03BA\u03B1\u03C4\u03AC\u03C3\u03C4\u03B1\u03C3\u03B7");
+		btnTrexousa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JSeparator separator = new JSeparator();
+		GridBagConstraints gbc_separator = new GridBagConstraints();
+		gbc_separator.insets = new Insets(0, 0, 5, 0);
+		gbc_separator.gridx = 0;
+		gbc_separator.gridy = 1;
+		frmFoodRiders.getContentPane().add(separator, gbc_separator);
+		GridBagConstraints gbc_btnTrexousa = new GridBagConstraints();
+		gbc_btnTrexousa.insets = new Insets(0, 0, 5, 0);
+		gbc_btnTrexousa.fill = GridBagConstraints.BOTH;
+		gbc_btnTrexousa.gridx = 0;
+		gbc_btnTrexousa.gridy = 2;
+		frmFoodRiders.getContentPane().add(btnTrexousa, gbc_btnTrexousa);
+		
+		JButton btnEstiatoria = new JButton("\u0395\u03C3\u03C4\u03B9\u03B1\u03C4\u03CC\u03C1\u03B9\u03B1");
+		GridBagConstraints gbc_btnEstiatoria = new GridBagConstraints();
+		gbc_btnEstiatoria.insets = new Insets(0, 0, 5, 0);
+		gbc_btnEstiatoria.fill = GridBagConstraints.BOTH;
+		gbc_btnEstiatoria.gridx = 0;
+		gbc_btnEstiatoria.gridy = 3;
+		frmFoodRiders.getContentPane().add(btnEstiatoria, gbc_btnEstiatoria);
+		
+		JButton btnParaggelies = new JButton("\u0399\u03C3\u03C4\u03BF\u03C1\u03B9\u03BA\u03CC \u03A0\u03B1\u03C1\u03B1\u03B3\u03B3\u03B5\u03BB\u03B9\u03CE\u03BD");
+		GridBagConstraints gbc_btnParaggelies = new GridBagConstraints();
+		gbc_btnParaggelies.insets = new Insets(0, 0, 5, 0);
+		gbc_btnParaggelies.fill = GridBagConstraints.BOTH;
+		gbc_btnParaggelies.gridx = 0;
+		gbc_btnParaggelies.gridy = 4;
+		frmFoodRiders.getContentPane().add(btnParaggelies, gbc_btnParaggelies);
+		
+		JButton btnYpalliloi = new JButton("\u039B\u03AF\u03C3\u03C4\u03B1 \u03A5\u03C0\u03B1\u03BB\u03BB\u03AE\u03BB\u03C9\u03BD");
+		btnYpalliloi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GuiEmployees ge = new GuiEmployees();
+				ge.EmployeesScreen();
+			}
+		});
+		GridBagConstraints gbc_btnYpalliloi = new GridBagConstraints();
+		gbc_btnYpalliloi.insets = new Insets(0, 0, 5, 0);
+		gbc_btnYpalliloi.fill = GridBagConstraints.BOTH;
+		gbc_btnYpalliloi.gridx = 0;
+		gbc_btnYpalliloi.gridy = 5;
+		frmFoodRiders.getContentPane().add(btnYpalliloi, gbc_btnYpalliloi);
+		
+		JButton btnStatistika = new JButton("\u03A3\u03C4\u03B1\u03C4\u03B9\u03C3\u03C4\u03B9\u03BA\u03AC");
+		GridBagConstraints gbc_btnStatistika = new GridBagConstraints();
+		gbc_btnStatistika.insets = new Insets(0, 0, 5, 0);
+		gbc_btnStatistika.fill = GridBagConstraints.BOTH;
+		gbc_btnStatistika.gridx = 0;
+		gbc_btnStatistika.gridy = 6;
+		frmFoodRiders.getContentPane().add(btnStatistika, gbc_btnStatistika);
+		
+		JSeparator separator_1 = new JSeparator();
+		GridBagConstraints gbc_separator_1 = new GridBagConstraints();
+		gbc_separator_1.insets = new Insets(5, 0, 5, 0);
+		gbc_separator_1.gridx = 0;
+		gbc_separator_1.gridy = 7;
+		frmFoodRiders.getContentPane().add(separator_1, gbc_separator_1);
+		
+		JButton btnAposindesi = new JButton("\u0391\u03C0\u03BF\u03C3\u03CD\u03BD\u03B4\u03B5\u03C3\u03B7");
+		GridBagConstraints gbc_btnAposindesi = new GridBagConstraints();
+		gbc_btnAposindesi.fill = GridBagConstraints.VERTICAL;
+		gbc_btnAposindesi.gridx = 0;
+		gbc_btnAposindesi.gridy = 8;
+		frmFoodRiders.getContentPane().add(btnAposindesi, gbc_btnAposindesi);
+	}
+}
