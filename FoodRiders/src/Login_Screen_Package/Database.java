d581859b98a5759109388e7857d2a00778b85a15
VaggosBt <37471733+VaggosBt@users.noreply.github.com>
2018-03-20 14:26:34 +0200

    ++

--- FoodRiders/src/Login_Screen_Package/Database.java
+++ FoodRiders/src/Login_Screen_Package/Database.java
@@ -4,33 +4,46 @@ import java.util.ArrayList;
 
 public class Database {
 
-	private ArrayList<User> users;
+	private ArrayList<User> administrators;
+	private ArrayList<User> clients;
+	private int LoginType;
 
 	public Database() {
 		
-		users = new ArrayList<>();
+		administrators = new ArrayList<>();
 		
 		User user1 = new User("Vaggos", "vaggos", "15/2/14");
-		User user2 = new User("Vicky", "vicky", "25/4/15");
+		User user2 = new User("Vicky", "vicky", "25/4/15");  
 		User user3 = new User("Tasos", "tasos", "31/5/17");
 		
-		users.add(user1);
-		users.add(user2);
-		users.add(user3);
+		administrators.add(user1);
+		administrators.add(user2);
+		
+		clients.add(user3);
 		
 	}
 
 	public boolean checkData(String aGivenUsername, String aGivenPassword){
-		
-		boolean flag = false;
-		
-		for(User aUser : users){
-			if(aUser.getName().equals(aGivenUsername)) {
-				flag = true;
+
+		for(User anAdmin : administrators){
+			if(anAdmin.getName().equals(aGivenUsername)) {
+				LoginType = 1;
+				return true;
 			}
 		}
-		return flag;
+		for(User aClient : clients) {
+			if(aClient.getName().equals(aGivenUsername)) {
+				LoginType = 2;
+				return true;
+			}
+		}
+		return false;
 	}
+
+	public int getLoginType() {
+		return LoginType;
+	}
+	
 	
 	
 }
