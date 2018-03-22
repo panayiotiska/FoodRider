8dd987c7d891dd9c882bd5985f1bc3a7fff31b6d
VaggosBt <37471733+VaggosBt@users.noreply.github.com>
2018-03-22 17:43:38 +0200

    commit

--- FoodRiders/src/Main/Main.java
+++ FoodRiders/src/Main/Main.java
@@ -9,7 +9,8 @@ public class Main {
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, 
 							 					  UnsupportedLookAndFeelException, MalformedURLException {
 		Login_Screen window = new Login_Screen();
-		window.showLoginScreen(); 
+		
+		window.showLoginScreen();
 
 	}
 
