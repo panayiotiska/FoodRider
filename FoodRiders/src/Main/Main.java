0233d2d6eedbce3a0f710c6af013aa9d2e51a898
VaggosBt <37471733+VaggosBt@users.noreply.github.com>
2018-03-23 17:12:20 +0200

    test commit

--- FoodRiders/src/Main/Main.java
+++ FoodRiders/src/Main/Main.java
@@ -3,7 +3,7 @@ package Main;
 import java.net.MalformedURLException;
 import javax.swing.UnsupportedLookAndFeelException;
 import Login_Screen_Package.Login_Screen;
-
+ 
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, 
