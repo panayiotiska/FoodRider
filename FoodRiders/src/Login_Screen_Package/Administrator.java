003ea70def73d11cd77c0b5f409f157040e1d79c
Panos <Panos@DESKTOP-GQH30VD>
2018-05-16 18:10:08 +0300

    fixed login bug

--- FoodRiders/src/Login_Screen_Package/Administrator.java
+++ FoodRiders/src/Login_Screen_Package/Administrator.java
@@ -1,28 +1,9 @@
 package Login_Screen_Package;
 
 public class Administrator extends User{
-
-	private String name; 
-	private String password;
-	private String subscriptionDate;
 	
 	public Administrator(String aName, String aPassword, String aSubscriptionDate) {
 		super(aName, aPassword, aSubscriptionDate);
 		
 	}
-
-	public String getName() {
-		return name;
-	}
-
-
-	public String getPassword() {
-		return password;
-	}
-
-	public String getSubscriptionDate() {
-		return subscriptionDate;
-	}
-
-	
 }
